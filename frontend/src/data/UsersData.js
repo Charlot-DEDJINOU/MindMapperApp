b7ed04eceb1cb162161ad031eabf66da8343b1af
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-05-02 18:10:05 +0100

    UsersData

---
+++ frontend/src/data/UsersData.js
@@ -0,0 +1,100 @@
+export default function UsersData() {
+    return [
+        {
+            id : 1,
+            first_name : "Charlot",
+            last_name : "DEDJINOU",
+            phone : "59105267",
+            email : "dedjinoucharlotjoel@gmail.com",
+            admin : true
+        },
+        {
+            id : 2,
+            first_name : "Mathias",
+            last_name : "KINNINKPO",
+            phone : "68615898",
+            email : "mathias@gmail.com",
+            admin : false
+        },
+        {
+            id : 3,
+            first_name : "Thierry",
+            last_name : "TCHONKLOE",
+            phone : "55669978",
+            email : "thierry@gmail.com",
+            admin : true
+        },
+        {
+            id : 4,
+            first_name : "Samuel",
+            last_name : "TOHOUEGNON",
+            phone : "61768952",
+            email : "samuel@gmail.com",
+            admin : false
+        },
+        {
+            id : 1,
+            first_name : "Charlot",
+            last_name : "DEDJINOU",
+            phone : "59105267",
+            email : "dedjinoucharlotjoel@gmail.com",
+            admin : true
+        },
+        {
+            id : 2,
+            first_name : "Mathias",
+            last_name : "KINNINKPO",
+            phone : "68615898",
+            email : "mathias@gmail.com",
+            admin : false
+        },
+        {
+            id : 3,
+            first_name : "Thierry",
+            last_name : "TCHONKLOE",
+            phone : "55669978",
+            email : "thierry@gmail.com",
+            admin : true
+        },
+        {
+            id : 4,
+            first_name : "Samuel",
+            last_name : "TOHOUEGNON",
+            phone : "61768952",
+            email : "samuel@gmail.com",
+            admin : false
+        },
+        {
+            id : 1,
+            first_name : "Charlot",
+            last_name : "DEDJINOU",
+            phone : "59105267",
+            email : "dedjinoucharlotjoel@gmail.com",
+            admin : true
+        },
+        {
+            id : 2,
+            first_name : "Mathias",
+            last_name : "KINNINKPO",
+            phone : "68615898",
+            email : "mathias@gmail.com",
+            admin : false
+        },
+        {
+            id : 3,
+            first_name : "Thierry",
+            last_name : "TCHONKLOE",
+            phone : "55669978",
+            email : "thierry@gmail.com",
+            admin : true
+        },
+        {
+            id : 4,
+            first_name : "Samuel",
+            last_name : "TOHOUEGNON",
+            phone : "61768952",
+            email : "samuel@gmail.com",
+            admin : false
+        }
+    ]
+}
