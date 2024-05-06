f7e8695038c493739924c87a3f8f6bfbc7558691
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-05-06 13:23:01 +0100

    Input Check

---
+++ frontend/src/components/commons/InputCheck.jsx
@@ -0,0 +1,8 @@
+export default function InputCheck({type, label, name, value, className}) {
+    return(
+        <div className="flex items-center">
+            <input type={type} name={name} value={value} className="block w-5 h-5 bg-primary"/>
+            <span className={`block mx-2 font-bold ${className}`}>{label}</span>
+        </div>
+    )
+}
