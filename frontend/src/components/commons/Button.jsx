e231b9378f4bbe97a29d3afa6e0336b57d7a1c68
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-05-08 00:22:44 +0100

    Input

--- frontend/src/components/commons/Button.jsx
+++ frontend/src/components/commons/Button.jsx
@@ -2,7 +2,7 @@ import ReloadIcon from '../icons/ReloadIcon'
 
 export default function Button({onClick, className, isLoading = false, children}) {
     return(
-        <button className={`bg-primary text-white py-2 px-3 rounded-md flex flex-row items-center ${className}`} onClick={onClick}>
+        <button className={`bg-primary text-white py-2 px-3 rounded-md flex flex-row items-center justify-center ${className}`} onClick={onClick}>
             {
                 isLoading ? (
                     <>
--- frontend/src/components/commons/Input.jsx
+++ frontend/src/components/commons/Input.jsx
@@ -2,27 +2,30 @@ import EyeIcon from "../icons/EyeIcon";
 import EyeSlashIcon from "../icons/EyeSlashIcon";
 import { useState } from "react";
 
-export default function InputPassword ({ label, name, value, placeholder, onChange, className }) {
+export default function InputPassword ({ label, type, name, value, placeholder, onChange, className }) {
 
     const [show, setShow] = useState(false)
 
   return (
     <div className={`w-full mb-3 flex flex-col  ${className}`}>
-        <label className='mb-1'>{label}</label>
+        <label className='mb-2'>{label}</label>
         <div className="flex items-center w-full rounded-md border text-black bg-gray-100 border-gray-300">
             <input
                 name={name}
-                type={show ? "text" : "password"}
+                type={show ? "text" : type}
                 placeholder={placeholder}
                 onChange={onChange}
                 value={value}
                 className="w-full rounded-md border border-slate-50 px-4 py-2 bg-gray-100  focus:outline-none focus:border-primary"
             />
-            <span className="mx-2 hover:cursor-pointer" onClick={() => setShow(!show)}>
-                {
-                    show ? <EyeIcon /> : <EyeSlashIcon />
-                }
-            </span>
+            {
+                type === "password" && 
+                <span className="mx-2 hover:cursor-pointer" onClick={() => setShow(!show)}>
+                    {
+                        show ? <EyeIcon /> : <EyeSlashIcon />
+                    }
+                </span>
+            }
         </div>
     </div>
   );
