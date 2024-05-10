f2889a9e8491c4b95552c94cd1da04d5f26321f4
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-05-10 10:32:24 +0100

    src/components/commons/Header.jsx

--- frontend/src/components/commons/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return(
-        <header className="fixed h-16 w-full bg-primary text-white text-xl flex justify-center items-center font-bold">
-            Explication Annagramme 
-        </header>
-    )
-}
