da1e09aeb1aaa28c926c26796ff16ccd5fb0885f
thierry-Tchonkloe <thierrytchonkloe79@gmail.com>
2024-05-16 20:15:59 +0100

    mise en place de la partie detail

--- frontend/src/views/Dashboard.jsx
+++ frontend/src/views/Dashboard.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import DashboardLayout from "../layouts/DashboardLayout";
 import Responses from "./Dashboard/Responses";
 import Users from "./Dashboard/Users";
+import DetailResponse from "./Dashboard/DetailResponse";
 
 export default function Dashboard(){
     return (
@@ -9,6 +10,7 @@ export default function Dashboard(){
         <Routes>
           <Route path="/responses" element={<Responses />} />
           <Route path="/users" element={<Users />} />
+          <Route path="/detail" element={ <DetailResponse />} />
           <Route path="*" element={<p>Momo</p>} />
         </Routes>
       </DashboardLayout>
