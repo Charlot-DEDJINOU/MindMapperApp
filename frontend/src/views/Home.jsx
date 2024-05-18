334ad54794864a8757f8fa492d8c5180a8886a48
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-05-18 18:03:30 +0100

    package chart.js

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,10 +1,18 @@
+import QuestionsData from "../data/QuestionsData"
+import InputQuestion from "../components/InputQuestion"
+
 export default function Home() {
 
     return(
         <section id="home">
-            <header className="fixed h-16 w-full bg-primary text-white text-xl flex justify-center items-center font-bold">
+            <header className="h-16 w-full bg-primary text-white text-xl flex justify-center items-center font-bold">
                 Explication Annagramme 
             </header>
+            <div className="w-full mt-16 flex items-center justify-center">
+               <div className="w-[500px]">
+                    <InputQuestion {...QuestionsData()[0]}/> 
+               </div>
+            </div>
         </section>
     )
 }
