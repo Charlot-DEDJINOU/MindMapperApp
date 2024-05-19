36b5a97c8260b136d1d7d39c37ee65ef849813c8
Techteamblt <kinninkpomathias@gmail.com>
2024-05-19 19:36:40 +0100

    add question model

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,4 +1,4 @@
-from mongoengine import Document, StringField, EmailField, EnumField
+from mongoengine import Document, StringField, EmailField, EnumField, IntField
 import enum
 
 class RoleEnum(enum.Enum):
@@ -6,16 +6,20 @@ class RoleEnum(enum.Enum):
     manager = "manager"
     # Ajoutez d'autres rôles au besoin
 
+
 class User(Document):
-    username = StringField(max_length=50, unique=True, required=True)
-    password = StringField(max_length=255, required=True)
-    full_name = StringField(max_length=255, required=True)
+    id = IntField(primary_key=True, required=True)
+    firstname = StringField(max_length=255, required=True)
+    lastname = StringField(max_length=255, required=True)
     email = EmailField(unique=True, required=True)
-    role = EnumField(RoleEnum, required=True)
+    phone = StringField(max_length=20, required=True)
+    is_admin = StringField(max_length=10, required=True, choices=['a5b1c4d3f2', 'v9w5x8y6z7'])
+    password = StringField(max_length=255, required=True)
     
     meta = {
         'indexes': [
-            'username',
-            'email'
+            'email',
+            'phone'
         ]
     }
+
