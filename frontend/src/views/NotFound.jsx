9580e3c1975f24ded9a81bfa5ba202f65bfb5ae2
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-05-19 15:30:06 +0100

    src/views/NotFound.jsx

--- frontend/src/views/NotFound.jsx
+++ frontend/src/views/NotFound.jsx
@@ -1,5 +1,12 @@
+import error from '../assets/404.jpg'
+
 export default function NotFound() {
-    return(
-        <section id="not-found">Not Found</section>
+    return (
+        <section
+            id="not-found"
+            className="w-full h-screen"
+            style={{ backgroundImage: `url(${error})` }}
+        >
+        </section>
     )
 }
