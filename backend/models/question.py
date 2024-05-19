b56c74d44ea993ffe75c3a782f31d5dbd052eeeb
Techteamblt <kinninkpomathias@gmail.com>
2024-05-19 19:36:28 +0100

    add question

--- backend/models/question.py
+++ backend/models/question.py
@@ -1,5 +1,5 @@
 import mongoengine as me
-from mongoengine import Document, StringField, EmailField, EnumField, IntField
+from mongoengine import Document, StringField, IntField
 
 # Connexion à MongoDB
 me.connect('your_database_name', host='your_mongodb_uri')
