8494a5fb410eafddf39165422604bb892fbc5c2e
Techteamblt <kinninkpomathias@gmail.com>
2024-05-31 17:30:58 +0100

    init add of main.py

--- backend/main.py
+++ backend/main.py
@@ -4,14 +4,29 @@ from fastapi.staticfiles import StaticFiles
 import os
 from dotenv import load_dotenv
 
-from database.base import connect_to_mongodb
-from routes.user import  user_router
+from database.base import init_db
+from routes.auth import auth_router
+from routes.user import user_router
+
 
 
 
 load_dotenv()
 
-app = FastAPI()
+app = FastAPI(
+    title="Mind Mapper App",
+    description="Une api pour la gestion d'utilisateurs pour un cabinet Psy",
+    version="1.0.0",
+    contact={
+        "name": "Your Name",
+        "url": "http://your-website.com/contact",
+        "email": "your-email@domain.com",
+    },
+    license_info={
+        "name": "MIT License",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+)
 
 
 # Middleware pour autoriser les requêtes CORS 
@@ -23,17 +38,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+routers = [auth_router, user_router]
+for router in routers:
+    app.include_router(router)
+
+
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.on_event("startup")
 async def startup_event():
-    mongogb= await connect_to_mongodb(os.getenv("DATABASE_NAME"))
+    init_db()
     print("Connexion à MongoDB établie")
     
 
-
-app.include_router(user_router)
 
 
 
