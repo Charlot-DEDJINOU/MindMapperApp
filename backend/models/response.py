42ba93f1662beaddcb6af6185b8605c94a08133a
Techteamblt <kinninkpomathias@gmail.com>
2024-05-31 17:30:58 +0100

    init add of models

--- backend/models/response.py
+++ backend/models/response.py
@@ -10,11 +10,20 @@ class Response(Document):
     id_personality = IntField(required=True)
     id_statistique = IntField(required=True)
     date = DateTimeField(default=datetime.utcnow, required=True)
-    
-    # Génération dynamique des champs A1, A2, ..., I3
-    for letter in 'ABCDEFGHI':
-        for number in range(1, 11):
-            locals()[f'{letter}{number}'] = StringField(max_length=255)
+    content = StringField(required=True)
+    A = StringField(max_length=255)
+    B = StringField(max_length=255)
+    C = StringField(max_length=255)
+    D = StringField(max_length=255)
+    E = StringField(max_length=255)
+    F = StringField(max_length=255)
+    G = StringField(max_length=255)
+    H = StringField(max_length=255)
+    I = StringField(max_length=255)
+    # # Génération dynamique des champs A1, A2, ..., I3
+    # for letter in 'ABCDEFGHI':
+    #     for number in range(1, 11):
+    #         locals()[f'{letter}{number}'] = StringField(max_length=255)
 
     meta = {
         'indexes': [
