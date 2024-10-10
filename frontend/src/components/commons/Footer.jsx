27308daeb1d7c7343eeaaa11deabba85141ac2cd
thierry-Tchonkloe <thierrytchonkloe79@gmail.com>
2024-10-10 15:25:45 +0100

    changement du nom en Ennéagramme

--- frontend/src/components/commons/Footer.jsx
+++ frontend/src/components/commons/Footer.jsx
@@ -2,10 +2,10 @@ export default function Footer() {
     return(
         <footer className="w-full bg-[#000000dd] flex items-center justify-center p-5 text-white">
             <div className="container flex flex-col items-center">
-                <b className="text-center mb-3 text-3xl text-secondary">Annagramme</b>
+                <b className="text-center mb-3 text-3xl text-secondary">Ennéagramme</b>
                 <p className="text-center"> L’être humain à le potentiel de devenir quelque chose d’autre - David Goggins</p>
                 <div className="copyright my-4">
-                    &copy; Copyright <strong><span>Annagramme </span></strong>All Rights Reserved 
+                    &copy; Copyright <strong><span>Ennéagramme </span></strong>All Rights Reserved 
                 </div>
                 <div className="text-center">
                     Created by
